188d1aeae65a998094f152604d6127a84f49a47d
JMR <86660909+j-breeze@users.noreply.github.com>
2021-09-14 18:50:36 -0800

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,4 +1,4 @@
 //This tells Leaflet to connect to the tile service at the URL and display them on the map.
 var map = L.map('map').setView([38, -95], 4);
-var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var basemapUrl = 'https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}';
 var basemap = L.tileLayer(basemapUrl).addTo(map);
