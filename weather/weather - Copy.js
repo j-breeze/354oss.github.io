310a6341c36efd8135bbf663a0f976b04c516ef6
JMR <86660909+j-breeze@users.noreply.github.com>
2021-12-09 14:05:20 -0900

    Update weather - Copy.js

--- weather/weather - Copy.js
+++ weather/weather - Copy.js
@@ -6,12 +6,15 @@ var basemap = L.tileLayer.provider('Stamen.Terrain').addTo(map);
 var mesoMarkersGroup=new L.LayerGroup();  
 
 //Get weather information from Mesowest for the state
-var tkn ="7c0eab19bffc4221af1eaf72b4b1237e"
+var tkn ="7c0eab19bffc4221af1eaf73b4b1237e"
 $.getJSON('https://api.synopticdata.com/v2/stations/latest',  
      {  
-          state:'ak',                   
+          within:1440,
+          obtimezone:'utc',
+          units:'english'
+          stid:'PAEI',
           latestobs:1,  
-          token:tkn  
+          token:tkn
      },   
      function (data)   
      {  //Loop through all the weather stations  
@@ -20,11 +23,11 @@ $.getJSON('https://api.synopticdata.com/v2/stations/latest',
            try{  
                 var stn = data.STATION[i];  
                 var dat = stn.OBSERVATIONS;  
-                var stnInfo =stn.NAME.toUpperCase();  
-                var elev=parseInt(stn.ELEVATION);            
-                stnInfo = "<b>Air Temp:&nbsp;</b>"+Math.round(dat.air_temp[1])+"&deg;F"+ "</br><b>Wind Speed:&nbsp;</b>"+Math.round(dat.wind_speed[1]* 1.150)+"MPH"+"</br>  
-                +<b>Wind Direction:&nbsp;</b>"+getCardinalDirection(Math.round(dat.wind_direction[1]))+"</br><b>Relative Humidity:&nbsp;</b>"+dat.relative_humidity[1]+"%"+"</br>  
-                +<b>Elevation:&nbsp;</b>"+elev+"&prime;";       
+                var stnInfo = stn.NAME.toUpperCase();  
+                //var elev=parseInt(stn.ELEVATION);            
+                //stnInfo = "<b>Air Temp:&nbsp;</b>"+Math.round(dat.air_temp[1])+"&deg;F"+ "</br><b>Wind Speed:&nbsp;</b>"+Math.round(dat.wind_speed[1]* 1.150)+"MPH"+"</br>  
+                //+<b>Wind Direction:&nbsp;</b>"+getCardinalDirection(Math.round(dat.wind_direction[1]))+"</br><b>Relative Humidity:&nbsp;</b>"+dat.relative_humidity[1]+"%"+"</br>  
+                //+<b>Elevation:&nbsp;</b>"+elev+"&prime;";       
                 //Add stations into Leaflet markers group  
                 L.marker(L.latLng(stn.LATITUDE,stn.LONGITUDE),{title:stn.NAME.toUpperCase()}).bindPopup(stnInfo).addTo(mesoMarkersGroup);  
            }    
@@ -43,3 +46,4 @@ $.getJSON('https://api.synopticdata.com/v2/stations/latest',
 });  
 //Add markers group to the Map  
 map.addLayer(mesoMarkersGroup);  
+
