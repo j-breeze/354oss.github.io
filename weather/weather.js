6aef7555a44e70bd7e2dee49ea24a002239e5b7e
JMR <86660909+j-breeze@users.noreply.github.com>
2021-12-08 16:00:53 -0900

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -18,42 +18,42 @@ var baseMaps = {
 
 // MesoWest API key = 7c0eab19bffc4221af1eaf73b4b1237e
 
- //Hold markers group  
- var mesoMarkersGroup=new L.LayerGroup();   
- //Get weather information from Mesowest for the state VA  
- $.getJSON('http://api.mesowest.net/stations?callback=?',  
-      {  
-           state:'va',                   
-           latestobs:1,  
-           token:'demoToken'  
-      },   
-      function (data)   
-      {  //Loop through all the weather stations  
-        for(var i=0;i<data.STATION.length;i++)  
+//Hold markers group  
+var mesoMarkersGroup=new L.LayerGroup();   
+//Get weather information from Mesowest for the state of AK  
+$.getJSON('https://api.synopticdata.com/v2/stations/latest?',  
+     {  
+          state:'ak',                   
+          latestobs:1,  
+          token:'7c0eab19bffc4221af1eaf73b4b1237e'  
+     },   
+     function (data)   
+     {  //Loop through all the weather stations  
+       for(var i=0;i<data.STATION.length;i++)  
+          {  
+           try{  
+                var stn = data.STATION[i];  
+                var dat = stn.OBSERVATIONS;  
+                var stnInfo =stn.NAME.toUpperCase();  
+                var elev=parseInt(stn.ELEVATION);            
+                stnInfo = "<b>Air Temp:&nbsp;</b>"+Math.round(dat.air_temp[1])+"&deg;F"+ "</br><b>Wind Speed:&nbsp;</b>"+Math.round(dat.wind_speed[1]* 1.150)+"MPH"+"</br>  
+                +<b>Wind Direction:&nbsp;</b>"+getCardinalDirection(Math.round(dat.wind_direction[1]))+"</br><b>Relative Humidity:&nbsp;</b>"+dat.relative_humidity[1]+"%"+"</br>  
+                +<b>Elevation:&nbsp;</b>"+elev+"&prime;";       
+                //Add stations into Leaflet markers group  
+                L.marker(L.latLng(stn.LATITUDE,stn.LONGITUDE),{title:stn.NAME.toUpperCase()}).bindPopup(stnInfo).addTo(mesoMarkersGroup);  
+           }    
+           catch(e)  
            {  
-            try{  
-                 var stn = data.STATION[i];  
-                 var dat = stn.OBSERVATIONS;  
-                 var stnInfo =stn.NAME.toUpperCase();  
-                 var elev=parseInt(stn.ELEVATION);            
-                 stnInfo = "<b>Air Temp:&nbsp;</b>"+Math.round(dat.air_temp[1])+"&deg;F"+ "</br><b>Wind Speed:&nbsp;</b>"+Math.round(dat.wind_speed[1]* 1.150)+"MPH"+"</br>  
-                 +<b>Wind Direction:&nbsp;</b>"+getCardinalDirection(Math.round(dat.wind_direction[1]))+"</br><b>Relative Humidity:&nbsp;</b>"+dat.relative_humidity[1]+"%"+"</br>  
-                 +<b>Elevation:&nbsp;</b>"+elev+"&prime;";       
-                 //Add stations into Leaflet markers group  
-                 L.marker(L.latLng(stn.LATITUDE,stn.LONGITUDE),{title:stn.NAME.toUpperCase()}).bindPopup(stnInfo).addTo(mesoMarkersGroup);  
-            }    
-            catch(e)  
-            {  
-                alert("Error! "+ e);  
-            }  
-           }   
-  })       
- .done(function()  
- {  
- })  
- .fail(function()  
- {       
-      alert("Could not access the MesoWest!");  
- });  
- //Add markers group to the Map  
- map.addLayer(mesoMarkersGroup);  
+               alert("Error! "+ e);  
+           }  
+          }   
+ })       
+.done(function()  
+{  
+})  
+.fail(function()  
+{       
+     alert("Could not access the MesoWest!");  
+});  
+//Add markers group to the Map  
+map.addLayer(mesoMarkersGroup);  
