bdc8af4306c662e61de6bbe8063c93adf513144a
JMR <86660909+j-breeze@users.noreply.github.com>
2021-12-08 17:57:39 -0900

    :)

---
+++ weather/weather.py
@@ -0,0 +1,12 @@
+import os
+import os 
+import folium
+from folium import plugins
+import rioxarray as rxr
+import earthpy as et
+import earthpy.spatial as es
+
+m = folium.Map(location=[64.6431, -147.0638])
+    tiles='Stamen Terrain'
+
+m
