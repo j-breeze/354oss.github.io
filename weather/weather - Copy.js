5eb0af76ee2e054c1efa2d3def1e26867c95991f
JMR <86660909+j-breeze@users.noreply.github.com>
2021-12-15 23:38:41 -0900

    Update weather - Copy.js

--- weather/weather - Copy.js
+++ weather/weather - Copy.js
@@ -14,49 +14,6 @@ var basemap = L.tileLayer.provider('Stamen.Terrain', {
 	bounds:[[46.654,-200.941],[76.701,-120.231]]
 }).addTo(map);
 
-//This sets the marker options for the stations!
-var stnMarkerWg = {
-	radius:10,
-	fillColor:"purple",
-	color:"black",
-	opacity:1,
-	fillOpacity:1
-};
-
-var stnMarkerOG = {
-	radius:10,
-	fillColor:"red",
-	color:"black",
-	opacity:1,
-	fillOpacity:1
-};
-
-var stnMarkerBad = {
-	radius:10,
-	fillColor:"yellow",
-	color:"black",
-	opacity:1,
-	fillOpacity:1
-};
-
-var stnMarkerGood = {
-	radius:10,
-	fillColor:"green",
-	color:"black",
-	opacity:1,
-	fillOpacity:1
-};
-
-var stnMarkerUkn = {
-	radius:10,
-	fillColor:"black",
-	color:"black",
-	opacity:1,
-	fillOpacity:1
-};
-
-var stnMarkerOptions = {};
-
 //This is where the script pulls the data from Synoptic (parent company of MesoWest). The URL defines the parameters of what is pulled.
 //Typical format is &[parameter]=[thing]. For instance, &units=metric sets the returned unit values to metric.
 //https://developers.synopticdata.com/mesonet/explorer/ lets you customize what the API delivers and gives you a custom URL automatically!
@@ -66,10 +23,39 @@ fetch("https://api.synopticdata.com/v2/stations/latest?&token=7c0eab19bffc4221af
 	})
 	.then(function(data) {
 		var jsonLayer = L.geoJSON(data, {
+			style: function(feature) {
+				var ECT = Math.round(35.74+(0.6215*feature.properties.air_temp)-(35.75*(feature.properties.wind_speed**0.16))+(0.4275*feature.properties.air_temp*((feature.properties.wind_speed/1.15077944802)**0.16)))
+				if (ECT > 0) {
+					return {
+						color: "green",
+						fillOpacity:1
+					};
+				} else if (ECT > -30) {
+					return {
+						color: "yellow",
+						fillOpacity:1
+					};
+				} else if (ECT > -50) {
+					return {
+						color: "red",
+						fillOpacity:1
+					};
+				} else if (ECT <= -50) {
+					return {
+						color: "purple",
+						fillOpacity:1
+					};
+				} else {
+					return {
+						color: "black",
+						fillOpacity:1
+					};
+				}	
+			},
+			
 			//This loops through each feature and applies a popup with ECT information if wind > 2.6 kts 
 			//The equation doesnt work below that wind speed!
 			onEachFeature: function (feature, jsonLayer) {
-			
 				if (feature.properties.air_temp == null) {
 					var uECT = "Unknown!";
 				} else if (feature.properties.wind_speed == null ) {
@@ -93,23 +79,10 @@ fetch("https://api.synopticdata.com/v2/stations/latest?&token=7c0eab19bffc4221af
 				};
 				
 				jsonLayer.bindPopup("<h3>"+"Station: "+feature.properties.stid+"</h3>"+"ECT: "+uECT+"<br>"+"Air Temp: "+airtemp+"<br>"+"Wind Speed: "+windspeed);
-				
-				// This sets the coloring based on the ECT.
-				if (uECT <= -50) {
-					var stnMarkerOptions = stnMarkerWg;
-				} else if (uECT <= -30) {
-					var stnMarkerOptions = stnMarkerOG;
-				} else if (uECT <= 0) {
-					var stnMarkerOptions = stnMarkerBad;
-				} else if (uECT > 0) {
-					var stnMarkerOptions = stnMarkerGood;
-				} else {
-					var stnMarkerOptions = stnMarkerUkn
-				};
 			},
 			//This tells the function to use the station marker options defined above.
 			pointToLayer: function (feature, latlng) {
-				return L.circleMarker(latlng, {stnMarkerOptions});	
+				return L.circleMarker(latlng, {});	
 			}
 		}).addTo(map);
 	})
@@ -122,16 +95,12 @@ fetch("Special_Use_Airspace.geojson")
 	.then(function(response) {
 		return response.json();
 	})
-	.then(function(setMoaStyle) {
-		return {
-			color:"red",
-			fillColor:"red",
-			fillOpacity:0
-		};
-	})	
 	.then(function(data) {
 		var moaLayer = L.geoJSON(data, {
-			style: setMoaStyle,
+			style: {
+				color: "red",
+				fillOpacity:0
+			},
 			onEachFeature: function (feature, moaLayer) {
 				moaLayer.bindPopup("<h3>"+feature.properties.NAME+"</h3>");
 			}
