bd666fbca66f359d7981227b224d7ee6b0010ef5
JMR <86660909+j-breeze@users.noreply.github.com>
2021-12-20 17:19:59 -0900

    Delete weather/make me scream directory

--- weather/make me scream/interactive_map.py
+++
@@ -1,7 +0,0 @@
-# MesoWest API Token: 7c0eab19bffc4221af1eaf73b4b1237e
-
-import folium
-
-map_1 = folium.Map(location=[64.6431, -147.0638], tiles='Stamen Terrain', zoom_start=8)
-
-map_1.save("map1.html")
--- weather/make me scream/weather.py
+++
@@ -1,8 +0,0 @@
-# MesoWest API Token: 7c0eab19bffc4221af1eaf73b4b1237e
-
-print("Hello!")
-
-m = folium.Map(location=[64.6431, -147.0638])
-
-
-m
